a622d38fc5beec1270f9e5a0958d9934c2b74a84
joshuasoftdev <123251561+joshuasoftdev@users.noreply.github.com>
2023-11-11 04:52:23 -0800

    MD previewer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { marked } from "marked";
+import "./App.css";
 
 function App() {
+  const [text, setText] = useState(`
+ # H1 Text
+## H2 text
+[Learn more about google:](https://google.com)
+<dl>
+<dt>Text</dt>
+<dd> Is something people use sometimes.</dd>
+
+<dt>Markdown in HTML</dt>
+<dd>Does *not* work **very** well. Use HRML <em>tags</em>.</dd>
+</dl>
+
+\` This is inline code\`
+
+\`\`\`
+function exampleOf()
+{
+   return multiLineCodeBlock;
+}
+
+\`\`\`
+
+
+- Code
+- All
+- Day
+
+> This is a block code 
+
+![ Useful Programmer](https://pbs.twimg.com/josh.png)
+
+**Bolded Text**
+  `);
+
+  marked.setOptions({
+    breaks: true,
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea
+        id="editor"
+        onChange={(event) => {
+          setText(event.target.value);
+        }}
+        value={text}
+      ></textarea>
+      <div
+        id="preview"
+        dangerouslySetInnerHTML={{
+          __html: marked(text),
+        }}
+      ></div>
     </div>
   );
 }
